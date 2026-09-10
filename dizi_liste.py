35ced76619916ed88ec174ec92d36724ddf87961
Ömer Burak Karadağ <omerkaradag5509@gmail.com>
2026-09-10 22:28:45 +0300

    Dizi_Liste.V1

---
+++ dizi_liste.py
@@ -0,0 +1,34 @@
+print("---DİZİLER VE LİSTELER---")
+"""print("1.LİSTE ELEMANLARINA ERİŞME")
+liste=["ömer","burak",233312041,"EEM","2.Ö"]
+print(liste[1])"""
+
+'''print("2.LİSTEDEKİ BİR ELEMANIN İNDİS NUMARASINI ÖĞRENME")
+liste=[3,7,4,'3','s']
+print(liste.index(3))
+print(liste.index("3"))'''
+
+"""print("3.LİSTE DİLİMLEME")
+L=[1,2,3,4]
+print(L[2:])
+L=[1,2,3,4,5,6,7,8]
+print(L[:2])
+print(L[::-1]) #listeyi ters çevirme"""
+
+"""print("4.ELEMAN EKLEME")
+L=[1,2,3,4]
+L.append(5)
+print(L)
+L.append("ö")
+print(L)
+L.insert(3,9)
+print(L)
+A=[]
+for i in range(0,5):
+    sayi=int(input("sayi giriniz:"))
+    A.append(sayi)
+print(A)"""
+
+"""print("5.LİSTENİN ELEMAN SAYISINI ÖĞRENME")
+L=[1,2,3,4,"a","b","c"]
+print(len(L))"""
