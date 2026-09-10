e466807129c864f2e28d3fda0881ebf0004e106d
Ömer Burak Karadağ <omerkaradag5509@gmail.com>
2026-09-10 22:24:20 +0300

    Değişkenler.V1

---
+++ degiskenler.py
@@ -0,0 +1,137 @@
+import keyword
+from keyword import kwlist
+
+"""print("----PRİNT KOMUTU----")
+print("Hello World. İ will be software master.")
+print("İ am electrical electronic engineer.")
+print("----1.DEGİSKEN ATAMA----")
+a=10
+b=15
+print(a,b)
+print(a*b)
+print(a-b)
+print(a+b)"""
+'''print(a/b)
+print(12*4)
+c=a+b
+print(c)
+carpım=a*b
+print(carpım)
+print("----2.DEGİSKEN İSİMLENDİRME----")
+print('1.KURAL:Bir degisken isminin ilk karakteri ya HARF ya da ALT CİZGİ (_) olmalidir.')
+#A=3
+_ali=34
+a_li=27
+print('2.KURAL:Bir degisken ismi icerisinde ALT CİZGİ (_) haric BOSLUK VEYA DİGER OZEL KARAKTERLER bulunmamalıdır.')
+print('ORNEK:adil?=54 kullanımı yanlıştır. Soru işareti özel karakterdir.')
+print('3.KURAL:Bir degisken ismi bir python komutu olamaz.')
+print('Bunlar;')
+import keyword
+keyword.kwlist
+print(kwlist)
+print('dir.')
+print('4.KURAL:Bir degisken isimlerinde turkce karakter kullanmamalıyız.(ç,ğ,ı,ş,ö,ü,Ç,Ğ,I,Ş,Ö,Ü).Ancak değişken içerisindeki atamalarda kullanabilirsiniz.')
+print('5.KURAL:Bir degisken küçük harf ile isimlendirildiyse aynı harfin büyüğü ile farklı bir değişken oluşturulabilir.Bu duruma dikkat edilmelidir.')
+print("----3.DEGİSKENLERİ BİRBİRİNE AKTARMA----")
+X=44
+Y=33
+print(X,Y)
+Z=X
+X=Y
+Y=Z
+print(X,Y)
+print('Pythonda bunun daha kolay bir yolu vardır. ')
+print(X,Y)
+X,Y=Y,X
+print(X,Y)
+print('----------')
+X=44
+Y=33
+Z=55
+print(X,Y,Z)
+X,Y,Z=Y,Z,X
+print(X,Y,Z)
+print('----4.DEĞİŞKENE BAŞLANGIÇ DEĞERİ ATAMA----')
+Cep=0
+Cep=Cep+10
+Cep=Cep+20
+Cep=Cep+30
+Cep=Cep+40
+print(Cep)'''
+"""print('----5.OPERATÖRLER----')
+A=15
+B=4
+print('+ toplama')
+print(A,'+',B,'=',A+B)
+print('- çıkarma')
+print(A,'-',B,'=',A-B)
+print('* çarpma')
+print(A,'*',B,'=',A*B)
+print('/ bölme')
+print(A,'/',B,'=',A/B)
+print('% kalan')
+print(A,'%',B,'=',A%B)
+print('** üs alma')
+print(A,'**',B,'=',A**B)
+print('// tam sayı bölme')
+print(A,'//',B,'=',A//B)"""
+#cep=0
+#cep=cep+10
+#print(cep)
+#cep+=10
+#print(cep)
+#cep-=5
+#print(cep)
+#cep/=3
+#print(cep)
+#cep=10
+#cep//=3
+#print(cep)
+#cep%=2
+#print(cep)
+'''print('----6.VERİ TİPLERİ----')
+print('int = Tamsayı Veri Tipi')
+print('float = Ondalıklı Sayı Veri Tipi')
+print('bool = Doğru Yanlış (İki Durum) Veri Tipi')
+print('str (string) = Karakter Veri Tipi')
+
+A=3
+B=4.5
+C=True
+D='Doğru'
+print(type(A))
+print(type(B))
+print(type(C))
+print(type(D))
+print(type('ÖMER'))
+print(type(4>2))
+print(type(4.5))
+print(type(3))
+X=(10>5)
+print(X)
+X=(5<3)
+print(X)
+print(3==3)'''
+'''print('----VERİ TİPİ DÖNÜŞÜMLERİ----')
+print(int(9.6)) #9.6 sayısını int veri tipine dönüştürmüştür.
+print(float(9)) #9 sayısını float veri tipine dönüştürmüştür.
+print(str('4'))
+print(str('4')+str('4'))
+print(float(4.5)+int(6.7)) #int veri tipindeki değeri 6 almıştır çünkü int tam sayılar için kullanılır.'''
+'''#SIRA SENDE
+#ÖRNEK 1 - X,Y VE Z İSMİNDEKİ 3 DEĞİŞKENE 'BTK' ATAMASI YAPINIZ.
+X=Y=Z ='BTK'
+print(X,Y,Z)'''
+'''#ÖRNEK 2 - BİR DEĞİŞKENİN TİPİNİ ÖĞRENMEK İÇİN HANGİ KOMUT KULLANILIR?
+A=7.4
+B='KELİME'
+C=True
+print(type(A))
+print(type(B))
+print(type(C))'''
+'''#ÖRNEK 3 -
+İ=0
+J=0
+İ=İ+J
+#YUKARIDAKİ SATIRI DAHA KISA BİR ŞEKİLDE YAZINIZ.
+İ+=J'''
