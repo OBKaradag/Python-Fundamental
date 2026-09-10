3bd743491f65f73ee2a9e8733732c45a29448ba6
Ömer Burak Karadağ <omerkaradag5509@gmail.com>
2026-09-10 22:30:01 +0300

    Koşul_Karşılaştırma.V1

---
+++ kosul_ve_karsilastirma.py
@@ -0,0 +1,34 @@
+print("\t3.Ders:KOŞUL VE KARŞILAŞTIRMA")
+#if = Tek koşula bağlı gerçekleşecek işlemler için
+#-Eğer param yeterse dondurma alacağım.
+#if else = Çift koşula bağlı gerçekleşecek işlemler için
+#-Aksi halde sakız alacağım.
+#if elif else = Çoklu koşula bağlı gerçekleşecek işlemler için
+#-Eğer param dondurmaya yetmeyip sodaya yeterse soda alacağım.
+#KARŞILAŞTIRMA OPERATÖRLERİ;
+# ==  EŞİTTİR
+# != EŞİT DEĞİLDİR
+# > BÜYÜKTÜR
+# < KÜÇÜKTÜR
+# >= BÜYÜK EŞITTIR
+# <= KÜÇÜK EŞİTTİR
+'''print(8==3) #Bool Veri tipi 
+print(8!=4) #Bool Veri tipi 
+print(8>3) #Bool Veri tipi 
+print(8<3) #Bool Veri tipi 
+print(8>=3) #Bool Veri tipi 
+print(8<=3) #Bool Veri tipi '''
+#MATIKSAL OPERATÖRLER (AND,OR,NOT)
+"""print((4<6)and(5>2)) #AND=Her ikiside doğru ise TRUE yanıtı gönderir.Diğer durumlarda FALSE gönderir.
+print((4<2)or(5>5)) #OR=Yalnızca bir ifade doğru olsa bile TRUE gönderir.Her ikiside yanlış ise FALSE gönderir.
+print(not(3>7)) #NOT=İfade doğru ise tersi olan FALSE'yi yanlış ise tersi olan TRUE'yi gönderir."""
+#if kullanımı örnek
+'''if 5==5:
+    print("eşittir")'''
+#if else kullanımı örnek (sayı tahmin etmek)
+"""a=20
+b=int(input('sayıyı tahmin ediniz:'))
+if a==b:
+    print("Doğru tahmin :)")
+else:
+    print("Yanlış tahmin :(")"""
