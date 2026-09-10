3e0c17202060f3bdbcaae6b923bed175212b2585
Ömer Burak Karadağ <omerkaradag5509@gmail.com>
2026-09-10 22:33:03 +0300

    Veri_İşlemleri.V1

---
+++ veri_islemleri.py
@@ -0,0 +1,24 @@
+print('----VERİ İŞLEMLERİ-----')
+#INPUT = Veri girişi için kullanırız.
+'''isim=input('İsminiz:')
+soyisim=input('Soyisminiz:')
+print(type(isim))
+yas=int(input('Yaşınız:'))
+print(isim,soyisim,yas,"yaşındadır.")
+print(type(yas))'''
+#Veri çıkışı için kullanabileceğimiz komutlar:
+#1-Print:Yukarıda bol bol kullandık.
+'''print('b','t','k')
+#SEP= çıktı mesajındaki karakterler arasına ayraç koymak için kullanılır.
+print('b','t','k',sep='.')
+print('b','t','k',sep='*')
+print("öbk"*3)'''
+#\n'den sonraki ifade alt satıra yazılır.
+'''print("ömer burak\nkaradağ")
+print("ömer\nburak\n20")'''
+#\t kelimeler arasında bir tab boşluk bıraktırır.
+'''print('Ömer\tKaradağ\n2005')'''
+'''sayi1=int(input('birinci sayı:'))
+sayi2=int(input("ikinci sayı:"))
+print("iki sayının toplamı:",sayi1+sayi2)'''
+"""print((" /_|\n('_)<|\n \_|"))"""
